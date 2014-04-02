727f46e8a9577e0a1141e3fce1535992c4e8a95c
LRNAB <f.hussain.r7@gmail.com>
2014-04-02 21:44:54 +0500

    Initial Commit

---
+++ Solver/Problems/001.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solver.Problems
+{
+    class _001
+    {
+        public static Int32 Solve()
+        {
+            var sum = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Solver/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = 0;
+            var stopwatch = new System.Diagnostics.Stopwatch();
+
+            stopwatch.Start();
+            solution = Problems._001.Solve();
+            stopwatch.Stop();
+
+            Console.WriteLine(solution);
+            Console.WriteLine(String.Concat("Took ", stopwatch.ElapsedMilliseconds, " ms"));
+            Console.ReadKey();
+        }
+    }
+}
