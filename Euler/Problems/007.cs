4c9017212b0b0cbdd4021cc6a5c313c3b59cb0da
LRNAB <rvaveo+lrnab@gmail.com>
2014-07-10 23:51:37 +0500

    More Refactoring. Removed unused refrences and using directives.

--- Euler/Problems/007.cs
+++ Euler/Problems/007.cs
@@ -15,9 +15,6 @@
 
 using Euler.Maths;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Euler.Problems
 {
--- Euler/Problems/010.cs
+++ Euler/Problems/010.cs
@@ -1,8 +1,5 @@
 ﻿using Euler.Maths;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Euler.Problems
 {
